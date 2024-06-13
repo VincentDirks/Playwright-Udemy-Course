ee97edaaa5058d7c17d9f90560d8a1e8f985456b
VincentDirks <vincent@dirksonline.net>
2024-06-13 15:17:54 +1200

    fetch a copy of playwright.config.ts from Bondar-artem's repo

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,64 +1,88 @@
-import { defineConfig, devices } from "@playwright/test"
-import type { TestOptions } from "./test-options"
-import { start } from "repl"
+import { defineConfig, devices } from '@playwright/test';
+import type { TestOptions } from './test-options';
 
-require("dotenv").config()
+require('dotenv').config();
 
 export default defineConfig<TestOptions>({
-  expect: {
-    // toMatchSnapshot: { maxDiffPixels: 50 },
+  timeout: 40000,
+  // globalTimeout: 60000,
+  expect:{
+    timeout: 2000,
+    toMatchSnapshot: {maxDiffPixels: 50}
   },
+
   retries: 1,
   reporter: [
-    ["json", { outputFile: "test-results/jsonReport.json" }],
-    ["junit", { outputFile: "test-results/junitReport.xml" }],
-    // ["allure-playwright"],
-    ["html"],
+    process.env.CI ? ["dot"] : ["list"],
+    [
+      "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+      },
+    ],
+    ['json', {outputFile: 'test-results/jsonReport.json'}],
+    ['junit', {outputFile: 'test-results/junitReport.xml'}],
+    // ['allure-playwright'],
+    ['html']
   ],
 
   use: {
-    baseURL: "http://localhost:4200",
-    globalsQaURL: "https://www.globalsqa.com/demo-site/draganddrop/",
+    globalsQaURL: 'https://www.globalsqa.com/demo-site/draganddrop/',
+    baseURL: process.env.DEV === '1' ? 'http://localhost:4201/'
+          : process.env.STAGING == '1' ? 'http://localhost:4202/'
+          : 'http://localhost:4200/',
 
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
+    screenshot: "only-on-failure",
     actionTimeout: 20000,
     navigationTimeout: 25000,
-    video: "on-first-retry",
+    video: {
+      mode: 'off',
+      size: {width: 1920, height: 1080}
+    }
   },
 
   projects: [
     {
-      name: "Dev",
-      use: {
-        ...devices["Desktop Chrome"],
-        baseURL: "http://the.dev.env.com",
-      },
-    },
-
-    { name: "chromium" },
-
-    {
-      name: "firefox",
-      use: { browserName: "firefox" },
+      name: 'dev',
+      use: { 
+        ...devices['Desktop Chrome'],
+        baseURL: 'http://localhost:4200/'
+       },
     },
     {
-      name: "pageObjectFullScreen",
-      testMatch: "usePageObjects.spec.ts",
-      use: {
-        viewport: { width: 1920, height: 1080 },
-      },
+      name: 'chromium',
     },
     {
-      name: "mobile",
-      testMatch: "testMobile.spec.ts",
+      name: 'firefox',
       use: {
-        ...devices["iPhone 13 Pro"],
-        // viewport: { width: 414, height: 800 },
-      },
+         browserName: 'firefox',
+         video: {
+          mode: 'off',
+          size: {width: 1920, height: 1080}
+        }
+        }
     },
+    {
+      name: 'pageObjectFullScreen',
+      testMatch: 'usePageObjects.spec.ts',
+      use: {
+        viewport: {width: 1920, height: 1080}
+      }
+    },
+    {
+      name: 'mobile',
+      testMatch: 'testMobile.spec.ts',
+      use: {
+        ...devices['iPhone 13 Pro']
+      }
+    }
   ],
+
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:4200/'
+    url: 'http://localhost:4200/',
+    timeout: 120 * 1000,
   }
-})
+});
